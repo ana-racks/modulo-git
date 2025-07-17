d1587a70f0f65eca80131d6d2fd493b1aed65e8c
Ana <ana@MACBOOKPRO-4209.station>
2025-07-17 18:42:25 +0200

    nueva línea

--- script2.js
+++ script2.js
@@ -1,5 +1,6 @@
 class name {
     constructor(parameters) {
         console.log("Constructor called with parameters:", parameters); 
+        console.log("This is a new log statement in script2.js");
     }
 }
